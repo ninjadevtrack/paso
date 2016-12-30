abc58715056b7d9b079a3def56ec30f718a63cf3
Lex Le <lle@oneearthfuture.org>
2016-12-30 05:55:14 -0500

    update sites

--- scripts/sync-static-files.sh
+++ scripts/sync-static-files.sh
@@ -1,1 +1,1 @@
-rsync -azP --exclude 'tmp/' --exclude 'private/' /var/www/dev.pasocolombia.org/htdocs/sites/default/files/ /var/www/pasocolombia.org/htdocs/sites/default/files/
+rsync -azP --exclude 'private/' --exclude 'tmp/' /var/www/dev.pasocolombia.org/htdocs/sites/default/files/ /var/www/pasocolombia.org/htdocs/sites/default/files/
