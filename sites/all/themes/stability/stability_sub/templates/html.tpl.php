58b692dca44754d4856a7865f7e3d71946f92c8f
Lex Le <sonle219@gmail.com>
2017-08-18 22:43:46 +0700

    remove GA code tracking

--- sites/all/themes/stability/stability_sub/templates/html.tpl.php
+++ sites/all/themes/stability/stability_sub/templates/html.tpl.php
@@ -85,8 +85,5 @@
     <link rel="stylesheet" href="<?php print base_path() . drupal_get_path('theme', 'stability'); ?>/css/style-switcher.css">
     <script src="<?php print base_path() . drupal_get_path('theme', 'stability'); ?>/js/style-switcher.js"></script>
   <?php endif; ?>
-  <script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-101987179-1', 'auto'); ga('send', 'pageview');
-</script>
 </body>
 </html>
