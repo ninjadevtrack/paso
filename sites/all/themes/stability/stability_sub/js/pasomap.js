846d0c88dae0ee2c1343ae8d791e6f593003266a
SocialRoot <dev@socialroot.co>
2019-11-07 07:06:29 -0800

    d3 mapping

--- sites/all/themes/stability/stability_sub/js/pasomap.js
+++ sites/all/themes/stability/stability_sub/js/pasomap.js
@@ -3,13 +3,63 @@
 const $ = jQuery;
 const themeURL = '/sites/all/themes/stability/stability_sub/';
 const mapID = 'pasomap';
+const mapHTML = `
+<div class="paso-container" id="paso-container">
+  <div class="content-header">
+    <h2 id="pasomap-title"></h2>
+  </div>
+  <div class="content">
+    
+    <div class="content-body">
+      <div class="map-wrapper d-flex">
+        
+        <div class="map-content">
+          <div class="map-content-section">
+            <div class="map-note"><p>Click circles on the map to see each ERA. Click outside the circles to return to national aggregate.</p></div>
+            <div class="datamap">
+              <div id="pasomap"></div>
+            </div>
+            <div class="map-footer">
+              <p class="map-footer-text">Phase <i class="map-circle"></i> Ongoing</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="map-meta d-flex">
+          <div class="meta-item projects">
+            <div class="meta-item-content">
+              <h4 class="meta-title">Productive Projects</h4>
+              <div class="meta-item-list"></div>
+            </div>
+          </div>
+          <div class="meta-item partners">
+            <div class="meta-item-content">
+              <h4 class="meta-title">Our Partners</h4>
+              <div class="meta-item-list"></div>
+            </div>
+          </div>
+          <div class="meta-item organizations">
+            <div class="meta-item-content">
+              <h4 class="meta-title">Beneficiary Organizations</h4>
+              <div class="meta-item-list"></div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    
+  </div>
+</div>
+`;
+const mapContainer = "#eramap";
+const eramapContainer = "#map-container";
 let __data = {},
     __areas = [],
     __markers = [];
 
-
-const $container = jQuery("#pasomap");
-const width = $container.width(), height = $container.height();
+const $_container = jQuery("#pasomap");
+const width = $_container.width(), height = $_container.height();
 const initialCenter = [4.570868, -74.297333]; // Colombia Center
 const mapStyles = [
   'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
@@ -21,11 +71,16 @@ const mapStyles = [
   'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 ];
-let currentAreaCode = '';
+let currentAreaCode = eraName;
 let sumParticipants = 0;
 let bubbleRadius = 11;
+let mapTitle = '';
 
 const raster_map = function() {
+  if ($(mapContainer).length == 0) return;
+  $(mapContainer).html(mapHTML);
+  $(eramapContainer).find(".col-right").css("height", $(mapContainer).height());
+
   let map = L.map(mapID).setView(initialCenter, 5);
   
   L.tileLayer(mapStyles[0], {
@@ -34,6 +89,15 @@ const raster_map = function() {
   }).addTo(map);
 
   L.svg().addTo(map);
+
+  map.on('click', function(e) {
+    if ($(e.originalEvent.target).hasClass("leaflet-zoom-animated")) {
+      $(`#${mapID} circle`).removeClass("inactive active");
+      // $(`#${mapID} circle`).addClass("active");
+      currentAreaCode = '';
+      refreshData();
+    }
+  });
   
   d3.csv(themeURL + 'data/areas.csv').then(function(data) {
     data.forEach((value, index) => {
@@ -56,7 +120,8 @@ const raster_map = function() {
       }
     });
     
-    $(".map-meta .participants .meta-item-list").html(`<h1>${sumParticipants}</h1>`);
+    updateMapTitle(sumParticipants);
+    // $(".map-meta .participants .meta-item-list").html(`<h1>${sumParticipants}</h1>`);
     
     // Add tooltip
     let tooltip = d3.select(`body`)
@@ -94,7 +159,7 @@ const raster_map = function() {
         .attr("id", function(d){ return d.area_code })
         .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).x })
         .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).y })
-        .attr("class", function(d){ return currentAreaCode == d.area_code ? 'active' : '' })
+        .attr("class", function(d){ return currentAreaCode == '' ? '' : currentAreaCode == d.area_code ? 'active' : 'inactive' })
         .attr("r", bubbleRadius)
         .style("fill", "#f0c86b")
         .attr("stroke", "#c18215")
@@ -165,8 +230,7 @@ const raster_map = function() {
         sumParticipants = parseInt(__areas[key].participants);
       }
     }
-    currentAreaCode == '' ? $(".region-name").text('NATIONAL AGGREGATE') : $(".region-name").text(__areas[currentAreaCode].era);
-    $(".map-meta .participants .meta-item-list").html(`<h1>${sumParticipants}</h1>`);
+    updateMapTitle(sumParticipants);
     
     ['organizations', 'projects', 'partners'].forEach(name => {
       let list = [];
@@ -196,6 +260,11 @@ const raster_map = function() {
       <div class="tt-row"><span class="tt-label">Average of LT</span><span class="tt-value">${d.lat}</span></div>
       <div class="tt-row"><span class="tt-label">Average of LN</span><span class="tt-value">${d.long}</span></div>
     </div>`;
+  }
+
+  function updateMapTitle(sumParticipants) {
+    currentAreaCode == '' ? mapTitle = 'NATIONAL AGGREGATE' : mapTitle = __areas[currentAreaCode]['era'];
+    $("#pasomap-title").html(`${mapTitle} | ${sumParticipants} PARTICIPANTS`);
   }
 
   readData("organizations", "Organización (2)");
--- sites/all/themes/stability/stability_sub/template.php
+++ sites/all/themes/stability/stability_sub/template.php
@@ -1,16 +1,35 @@
 <?php
 
+function parseEraName($node) {
+  $field_item = field_get_items('node', $node, 'field_era_name');
+  if (isset($field_item) && is_array($field_item)) {
+    return $field_item[0]["value"];
+  } else {
+    return "all";
+  }
+}
+
 function stability_sub_preprocess_page(&$variables) {
-
   $node = menu_get_object();
-
-  if ($node && ($node->nid == 242 || $node->nid == 243)) {
+  
+  if ($node && ($node->type == "era")) {
     drupal_set_title('');
     
     drupal_add_css(drupal_get_path('theme', 'stability_sub') . '/css/leaflet.css');    
     drupal_add_js('//d3js.org/d3.v5.js', 'external');
     drupal_add_js(drupal_get_path('theme', 'stability_sub') . '/js/leaflet.js');
-    drupal_add_js(drupal_get_path('theme', 'stability_sub') . '/js/pasomap.js');
-  }
 
+    $eraName = parseEraName($node);
+    drupal_add_js("var eraName='{$eraName}';", 'inline');
+    drupal_add_js(
+      url(
+        drupal_get_path('theme', 'stability_sub') . "/js/pasomap.js", 
+        array(
+          'query' => array(
+            'era' => $eraName
+          )
+        )
+      )
+    );
+  }
 }
